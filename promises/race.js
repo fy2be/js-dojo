806d97dff0ca4d0e2fd5b3364f584ed263ce619a
fy2be <36625528+fy2be@users.noreply.github.com>
2018-06-10 16:13:05 +0200

    Promise.race()

---
+++ promises/race.js
@@ -0,0 +1,33 @@
+var promise1 = new Promise(function (resolve, reject) {
+    console.log('[promise1]');
+    setTimeout(function () {
+        resolve('A');
+    }, 1000);
+});
+
+var promise2 = new Promise(function (resolve, reject) {
+    console.log('[promise2]');
+    setTimeout(function () {
+        resolve('B');
+    }, 2000);
+});
+
+var promise3 = new Promise(function (resolve, reject) {
+    console.log('[promise3]');
+    setTimeout(function () {
+        resolve('C');
+    }, 5000);
+});
+
+var promise4 = new Promise(function (resolve, reject) {
+    console.log('[promise4]');
+    setTimeout(function () {
+        reject(Error('X'));
+    }, 3000);
+})
+
+Promise.race([promise1, promise2, promise3, promise4]).then(function (result) {
+    console.log('[then]: ' + result);
+}).catch(function (result) {
+    console.log('[catch]: ' + result);
+});
